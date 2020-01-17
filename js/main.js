4f61f6d38e5b9b7d473b701d989ccdfd6b318c31
Kate <Compton@Main.local>
2020-01-17 08:28:51 -0600

    adding malicious data

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@
 function Hashtable() {
 	this.buckets = []
 
+	this.setSize(10)
+
 }
 
 Hashtable.prototype.setSize = function(size) {
-	this.buckets = []
-	for (var i = 0; i < size; i++) {
-		this.buckets[i] = []
+	while(this.buckets.length > 0) {
+		this.buckets.pop()
 	}
+	
+	for (var i = 0; i < size; i++) {
+		this.buckets.push([])
+	}
+	console.log(this)
 }
 
 Hashtable.prototype.clear = function() {
@@ -19,15 +25,17 @@ Hashtable.prototype.clear = function() {
 }
 
 Hashtable.prototype.add = function(hash, item) {
+
 	
-	let bucket = ((hash%this.buckets.length) + this.buckets.length)%this.buckets.length
-	console.log("Add " + item.id + " at " + bucket)
-	this.buckets[bucket].push(item)
+	let index = ((hash%this.buckets.length) + this.buckets.length)%this.buckets.length
+	
+	this.buckets[index].push(item)
+	
 }
 
 Hashtable.prototype.get = function(hash, id) {
 
-	let bucket = ((hash%this.buckets.length) + this.buckets.length)%this.buckets.length
+	let index = ((hash%this.buckets.length) + this.buckets.length)%this.buckets.length
 	
 	for (var i = 0; i < bucket.length; i++) {
 		if (bucket[i].id === id)
@@ -40,32 +48,42 @@ Hashtable.prototype.get = function(hash, id) {
 // What does this software do?
 // Given one dataset, compare how fast two storage methods can store and retrieve it
 
+Vue.component('bucket', {
+	props: ["index", "items"],
+	template: `<div class="hashbucket">
+					
+				<div class="hashbucket-label">
+					{{items.length}}
+				</div>
+				
+				<div class="hashbucket-item" :key="index" v-for="(item, index) in items">
+					{{item.key}} ({{item.val}})
+				</div>
 
+			</div>`
+})
 
 Vue.component('hashtable', {
-	properties: {
-		table: Array
+	props: {
+		table: Object
 	},
-  data: function () {
-    return {
-      count: 0
-    }
-  },
+
+	computed: {
+		buckets: function() {
+			return this.table.buckets}
+	},
+	 
   template: `<div class="hashtable">
 
+  		<div class="controls">
+  			table ({{buckets.length}} buckets)
+  		</div>
+  		<div class="bucketholder">
 
-			<div class="hashbucket" v-for="(bucket,index) in table">
-				
-				<div class="hashbucket-label">
-					{{index}}
-				</div>
-				
-				<div class="hashbucket-item" v-for="item in bucket">
-					{{item}}
-				</div>
-
-			</div>
-		</div>`
+			<bucket :key=index :index=index :items="buckets[index]" v-for="(bucket,index) in buckets"></bucket>
+			
+		</div>
+	</div>`
 })
 
 var app2 = new Vue({
@@ -77,18 +95,20 @@ var app2 = new Vue({
 			Hash function:<br><textarea v-model="hashFxnString" cols="40" rows="9"/>
 			
 			</div>
+
 			<div class="control-column">
-			Buckets: <input v-model="bucketCount"></input>
-			<br>
-			Test data:
-			<br>
-			<textarea v-model="testInput"/> 
-			<br>
-			<span>Hash: {{testHash}}</span>
-			<br>
-			<span>Bucket #{{testBucket}}</span>
+				Buckets: <input v-model="bucketCount"></input>
+				<br>
+				Test data:
+				<br>
+				<textarea v-model="testInput"/> 
+				<br>
+				<span>Hash: {{testHash}}</span>
+				<br>
+				<span>Bucket #{{testBucket}}</span>
 			</div>
-			<div id="coinstuff">
+
+			<div id="coinstuff" class="control-column">
 				<input id="coincheck" type="checkbox" v-model="mineBitcoins"/><label for="coincheck">Mine bitcoins?</label>
 				
 				<div v-if="mineBitcoins">
@@ -96,21 +116,25 @@ var app2 = new Vue({
 					<div>
 						Bitcoin function:<br><textarea v-model="bitcoinString" cols="40" rows="3"/>
 					</div>
+					
 					Bitcoins found: {{coins.length}}
-					<div v-if="mineBitcoins">
-						<div v-for="coin in coins">{{coin.id}} ({{coin.hash}})</div>
+
+					<div v-if="mineBitcoins" class="bitcoin-list">
+						<div v-for="coin in coins">{{coin.val}} {{coin.id}} ({{coin.hash}}) </div>
 					</div>
 				</div>
 			</div>
-			<div>
+
+			<div class="control-column">
 				<select v-model="dataID" @change="loadData">
 					<option v-for="(set,setID) in dataSets">{{setID}}</option>
 				</select>
 				<div>{{dataItems.length}} loaded, e.g:{{dataExample}}</div>
 				<button @click="addData">add all!</button>
+				Delay:<input v-model="delay" style="width:20px;"/> 
 			</div>
 		</div>
-		
+		<hashtable :table="table"></hashtable>
 	</div>`,
 
 	methods: {
@@ -119,7 +143,7 @@ var app2 = new Vue({
 		addData: function() {
 			console.log("add data of type", this.dataID)
 
-
+			// this.table.clear()
 
 				let count = 0
 
@@ -129,36 +153,57 @@ var app2 = new Vue({
 				if (this.delay > 0) {
 					setInterval(() => {
 
-						if (count < 30) {
-							let item = this.dataItems[count]
-							item.id = settings.id(item)
+						if (count < this.dataItems.length) {
 
+							let item = this.dataItems[count]
+							if (item) {
+
+							item.id = settings.id(item)
+							item.val = settings.val(item)
+							
 							
 							let hash = this.hashFxn(item.id)
-								console.log(item.id, hash)
 							// Add it to the hashtable using the function
 							this.table.add(hash, item)
 							if (this.coinFxn(hash)) {
 								this.coins.push({
 									hash: hash,
-									id: item.id
+									id: item.id,
+									val: item.val
 								})
 							}
 							count++
-
+							}
 
 						}
 						
 					}, this.delay)
 				} else {
-					console.log("add at once")
-					for (var i = 0; i < 100; i++) {
+					
+					for (var i = 0; i < this.dataItems.length; i++) {
+						let item = this.dataItems[i]
 
+						item.id = settings.id(item)
+						item.val = settings.val(item)
+						
+						
+						let hash = this.hashFxn(item.id)
+						// Add it to the hashtable using the function
+						this.table.add(hash, item)
+						if (this.coinFxn(hash)) {
+							this.coins.push({
+								hash: hash,
+								id: item.id,
+								val: item.val
+							})
+						}
 					}
 
 				}
 		},
 		loadData: function() {
+			this.coins = []
+
 			console.log(`get data set for `, this.dataID)
 	
 
@@ -166,6 +211,16 @@ var app2 = new Vue({
 			$.getJSON("data/" + this.dataID + ".json", (json) => {
 					console.log("GOT DATA: ", json.length, json[0])
 					this.dataItems = json
+					if (this.dataID == "malicious") {
+						this.dataItems = this.dataItems.map(item => {
+							return {
+							id: item
+						}}
+						)
+						console.log(this.dataItems)
+					}
+						
+
 					this.dataExample = json[Math.floor(Math.random()*json.length)]
 				
 			});
@@ -187,7 +242,7 @@ var app2 = new Vue({
 
 		coinFxn : function() {
 			let fxn = eval(this.bitcoinString)
-			
+			this.coins = []
 			return fxn
 		},
 
@@ -207,20 +262,20 @@ var app2 = new Vue({
 	},
 
 	watch: {
-		
-			bucketCount: () => this.table.setSize(this.bucketCount)
+	
+		bucketCount: function() { this.table.setSize(this.bucketCount)}
 	
 	},
 
 	mounted: function() {
 		this.loadData()
-		this.table.setSize(this.bucketCount)
+		// this.table.setSize(this.bucketCount)
 	},
 
 
 
 	data: {
-		delay: 10,
+		delay: 0,
 		mineBitcoins: true,
 		coins: [],
 		bitcoinString: `(s) => s%10 == 0`,
@@ -229,7 +284,7 @@ var app2 = new Vue({
 		hashFxnString: `(s) => {
 	let val = 0;
 	for (var i = 0; i < s.length; i++) {
-		val += s.charCodeAt(0);
+		val += s.charCodeAt(i);
 	}
 	return val;
 }`,
@@ -245,13 +300,17 @@ var app2 = new Vue({
 				id: (obj) => obj[0],
 				val: (obj) => obj[1]
 			},
+			"malicious": {
+				id: (obj) => obj.id,
+				val: (obj) => obj.id
+			},
 			"emoji": {
 				id: (obj) => obj.name,
 				val: (obj) => obj.char
 			},
 			"nyc_dogs": {
-				id: (obj) => obj.no,
-				val: (obj) => obj.name + "(" + obj.breed + ")"
+				id: (obj) => obj[1] + obj[0],
+				val: (obj) => obj[1] + "(" + obj[4] + ")"
 			},
 			"pokedex": {
 				id: (obj) => obj.name.english,
@@ -264,10 +323,4 @@ var app2 = new Vue({
 		message: 'You loaded this page on ' + new Date().toLocaleString()
 	}
 })
-
-// Define a new component called todo-item
-Vue.component('hashtable', {
-	template: '<div class="hashtable"></div>'
-})
-
 
